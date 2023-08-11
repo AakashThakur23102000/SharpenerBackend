337ed33da377d7b4961c22a4c5ac78400e1b412f
AakashThakur23102000 <aakashthakur20001972@gmail.com>
2023-08-12 04:07:53 +0530

    Clean Up Code

---
+++ Kickstarting Node Js/Clean Up Code/routes.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+
+var formHandler = (req, res) => {
+    // for home page
+    if (req.url === "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.write(
+            `
+            <html>
+                <body>
+                    <form action="/message" method="POST"> 
+                        <input type="text" name="message" placeholder="Enter some text" />
+                        <button type="submit">Submit</button>
+                    </form>
+                </body>
+            </html>
+            `
+        )
+        return res.end();
+    }
+
+    // for message page
+    if (req.url === "/message" && req.method === "POST") {
+        var body = [];
+        // now here the request contain data so we add a listner
+        // listenere ends at the end of the syncronised compilation is done after that it runs async code that why we 
+        // return our last end
+        req.on("data", (chunk) => {
+            body.push(chunk)
+        })
+        console.log(body);
+        return req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString().split("=");
+            fs.writeFile("message.txt", parsedBody[1], (err) => {
+                res.statusCode = 302;
+                res.setHeader("Location", "/");
+                return res.end();
+            });
+        })
+    }
+
+
+
+    res.setHeader("Content-Type", "text/html");
+    res.write(`<h1>This is home page</h1>`)
+    res.end();
+}
+
+module.exports = formHandler;
