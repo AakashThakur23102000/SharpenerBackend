5ba7126c0d436ad9fd48e8cd038a9151e216e279
AakashThakur23102000 <aakashthakur20001972@gmail.com>
2023-08-13 00:46:31 +0530

    Intro to Middlewares

---
+++ Starting with Express Framework/Lets get started with Express/app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const express = require("express");
+
+// express is a function which we run and store it in app
+const app = express();
+
+// req and res are same 
+// next is function called to pass the compiler to next to 
+app.use((req,res,next)=>{
+    console.log("I am first middleware");
+    next();
+})
+app.use((req,res,next)=>{
+    res.send("<h1>Page 1</h1>")
+    console.log("I am second middleware")
+})
+
+app.listen(4000);
